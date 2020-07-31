1ddcdf9d00c3eb7dde6186b49f8062df542c097f
junminChang <chjm219@kookmin.ac.kr>
2020-07-31 11:50:51 +0900

    stopWatch_MainActivity_1

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.timer
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    private var time = 0
+    private var timerTask: Timer? = null
+
+
+    private fun start() {
+        fab.setImageResource(R.drawable.ic_baseline_pause_24)
+
+
+        timerTask = timer(period = 10) {
+            time++
+
+            val sec = time / 100
+            val milli = time % 100
+
+            runOnUiThread {
+                secTextView.text = "$sec"
+                milliTextView.text = "$milli"
+            }
+        }
+
+
     }
 }
+
+
