8586f410acee5db087f8212834a5489e9c3d6694
junminChang <chjm219@kookmin.ac.kr>
2020-07-31 22:21:38 +0900

    stopWatch_final

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,21 +2,47 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.timer
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var time = 0
+    private var timerTask: Timer? = null
+    private var isRunning = false
+    private var lap = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-    }
+        fab.setOnClickListener {
+            isRunning = !isRunning
 
-    private var time = 0
-    private var timerTask: Timer? = null
+            if (isRunning) {
+                start()
+            } else {
+                pause()
+            }
+        }
+
+
+        lapButton.setOnClickListener {
+
+            recordLapTime()
+        }
+
+        resetFab.setOnClickListener {
+            reset()
+        }
+
+
+    }
 
 
     private fun start() {
@@ -34,9 +60,36 @@ class MainActivity : AppCompatActivity() {
                 milliTextView.text = "$milli"
             }
         }
-
-
     }
+
+    private fun pause() {
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        timerTask?.cancel()
+    }
+
+    private fun recordLapTime() {
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.text = "$lap LAP: ${lapTime / 100}.${lapTime % 100}"
+
+        lapLayout.addView(textView, 0)
+        lap++
+    }
+
+
+    private fun reset() {
+        timerTask?.cancel()
+
+        time = 0
+        isRunning = false
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        secTextView.text = "0"
+        milliTextView.text = "00"
+
+        lapLayout.removeAllViews()
+        lap = 1
+    }
+
 }
 
 
